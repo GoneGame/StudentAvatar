8ba599a7b00666fddf162929566bce79a708cdc0
GoneGame <doom_lol97@outlook.com>
2018-05-05 21:50:09 +0800

    variables to the login activity(MainActivity)

--- app/src/main/java/student/studentavater/MainActivity.java
+++ app/src/main/java/student/studentavater/MainActivity.java
@@ -2,14 +2,30 @@ package student.studentavater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //lololol testing!!
-        //testing home pc
+
+        username = (EditText)findViewById(R.id.editLogin);
+        password = (EditText)findViewById(R.id.editPass);
+        login = (Button)findViewById(R.id.buttonLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
