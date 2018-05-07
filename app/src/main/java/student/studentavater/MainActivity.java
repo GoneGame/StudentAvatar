5d7cb82e58f4529b07b887a6e7d844a4284f8d99
Doom <doomlolxxiv@gmail.com>
2018-05-07 09:21:33 +0800

    added volley, skeleton for login

--- app/src/main/java/student/studentavater/MainActivity.java
+++ app/src/main/java/student/studentavater/MainActivity.java
@@ -6,10 +6,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText username, password;
-    Button login;
+    private EditText username, password;
+    private Button login;
+    private RequestQueue requestQueue;
+    private static final String URL = "http://172.17.8.47/websiteapp/php/login_app.php";
+    private StringRequest stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +37,48 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.editPass);
         login = (Button)findViewById(R.id.buttonLogin);
 
+        requestQueue = Volley.newRequestQueue(this);
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            if(jsonObject.names().get(0).equals("success"))
+                            {
+                                //add the action to open new activity and toast or log message
+                            }
+                            else
+                            {
+                                //show error
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String, String> hashMap = new HashMap<String, String>();
+                        hashMap.put("user_name", username.getText().toString());
+                        hashMap.put("user_pass", password.getText().toString());
+                        return hashMap;
+                    }
+                };
+
+                requestQueue.add(stringRequest);
 
             }
         });
