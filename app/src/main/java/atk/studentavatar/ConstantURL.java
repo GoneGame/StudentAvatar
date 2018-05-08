69adb1ef44b96e3db74c636f35797aca1edf407c
Doom <doomlolxxiv@gmail.com>
2018-05-08 13:33:41 +0800

    ConstantURL class to store links to php scripts in xampp

---
+++ app/src/main/java/atk/studentavatar/ConstantURL.java
@@ -0,0 +1,7 @@
+package atk.studentavatar;
+
+public class ConstantURL {
+    private static final String ROOT_URL = "http://172.17.8.47/websiteapp/php/";
+    public static final String URL_LOGIN = ROOT_URL + "login_app.php";
+    public static final String URL_GET_EVENTS = ROOT_URL + "some.php";
+}
