236db215b03cba789ca72fc0f3fa0804d3593594
GoneGame <doom_lol97@outlook.com>
2018-05-09 00:36:06 +0800

    updated constant url class, added volley function

--- app/src/main/java/atk/studentavatar/ConstantURL.java
+++ app/src/main/java/atk/studentavatar/ConstantURL.java
@@ -1,7 +1,7 @@
 package atk.studentavatar;
 
 public class ConstantURL {
-    private static final String ROOT_URL = "http://172.17.8.47/websiteapp/php/";
+    private static final String ROOT_URL = "http://172.17.8.47/websiteapp/php/app";
     public static final String URL_LOGIN = ROOT_URL + "login_app.php";
-    public static final String URL_GET_EVENTS = ROOT_URL + "some.php";
+    public static final String URL_GET_EVENTS = ROOT_URL + "get_events_app.php";
 }
--- app/src/main/java/atk/studentavatar/fragment/CalendarListFragment.java
+++ app/src/main/java/atk/studentavatar/fragment/CalendarListFragment.java
@@ -10,12 +10,22 @@ import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import atk.studentavatar.ConstantURL;
 import atk.studentavatar.R;
 
 public class CalendarListFragment extends Fragment {
     private LinearLayoutManager mManager;
     private CalendarView calendarView;
-    private int year, month, date;
+    private int year, month, day;
 
     public CalendarListFragment() {}
 
@@ -28,7 +38,7 @@ public class CalendarListFragment extends Fragment {
         Bundle bundle = getArguments();
         year = bundle.getInt("Year");
         month = bundle.getInt("Month");
-        date = bundle.getInt("Day");
+        day = bundle.getInt("Day");
         //and finally the username
 
         bundle.clear();
@@ -47,7 +57,29 @@ public class CalendarListFragment extends Fragment {
 
     private void queryToMySQLserver()
     {
-        //volley script here
+        //write the php script
+        final String username = "put from current user";
+        final String date = Integer.toString(year) + "-" + Integer.toString(month) + "-" + Integer.toString(day);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ConstantURL.URL_GET_EVENTS, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("username", username);
+                map.put("date", date);
+            }
+        };
+
     }
 
     @Override
